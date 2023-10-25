86cbaf454c2bcaecf2e6540f5c12c142dd97d7e2
Renato Paiva <paivarenato08@gmail.com>
2023-10-25 12:51:29 -0300

    Instalando biblioteca , integrando projeto com IA ChatGPT e exibindo resumo de uma Banda

--- ScreenSound/Menus/MenuRegistrarBanda.cs
+++ ScreenSound/Menus/MenuRegistrarBanda.cs
@@ -1,4 +1,5 @@
-﻿using ScreenSound.Modelos;
+﻿using OpenAI_API;
+using ScreenSound.Modelos;
 
 namespace ScreenSound.Menus;
 
@@ -12,8 +13,20 @@ internal class MenuRegistrarBanda : Menu
         string nomeDaBanda = Console.ReadLine()!;
         Banda banda = new Banda(nomeDaBanda);
         bandasRegistradas.Add(nomeDaBanda, banda);
+
+        var client = new OpenAIAPI("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");
+
+        var chat = client.Chat.CreateConversation();
+
+        chat.AppendSystemMessage($"Resuma a banda {nomeDaBanda} em 1 parágrafo.Adote um estilo informal.");
+
+        string resposta = chat.GetResponseFromChatbotAsync().GetAwaiter().GetResult();
+        banda.Resumo = resposta;
+
         Console.WriteLine($"A banda {nomeDaBanda} foi registrada com sucesso!");
-        Thread.Sleep(4000);
+        Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+        Console.ReadKey();
         Console.Clear();
+
     }
 }
